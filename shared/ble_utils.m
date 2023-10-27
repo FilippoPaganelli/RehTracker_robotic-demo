64c9e3570bd6209ecda1cf8eb101a9dfc292154b
FilippoPaganelli <32205866+FilippoPaganelli@users.noreply.github.com>
2023-10-27 21:42:57 +0200

    added comments & clean-up

--- shared/ble_utils.m
+++ shared/ble_utils.m
@@ -1,19 +1,16 @@
-
+% clear workspace from previously connected devices
 clear device;
 
+% connect to physical prototype via device ID
 device = ble("021E7DE0C1CB");
 
-%
-
-% ch_type = characteristic(device, "0CCC7966-1399-4C67-9EDE-9B05DBEA1BA2", "B964A50A-0002-4D37-97EB-971BF5233A98");
+% define BLE characteristics
 ch_data = characteristic(device, "0CCC7966-1399-4C67-9EDE-9B05DBEA1BA2","B964A50A-0001-4D37-97EB-971BF5233A98" );
 ch_robot = characteristic(device, "0CCC7966-1399-4C67-9EDE-9B05DBEA1BA2", "B964A50A-0003-4D37-97EB-971BF5233A98");
 
-%
-
+% write to physical prototype to switch to robotics demo mode
 is_robot = 1;
 write(ch_robot, is_robot);
 
-%
-
+% assign custom callback to handle the notified data from the Arduino
 ch_data.DataAvailableFcn = @readBleDataCallback;
--- shared/readBleDataCallback.m
+++ shared/readBleDataCallback.m
@@ -1,9 +1,15 @@
+% data callback definition
+
 function readBleDataCallback(src, arg)
+    % reading raw data
     raw_data = read(src,'oldest');
 
+    % converting raw data to a double in [0,1]
     data = double(raw_data(1)/100);
 
+    % assign converted data to the base workspace
     assignin("base", "data", data);
     
+    % set the new data value as input to the Simulink model
     set_param("final_simulink_model_V3/EMG Value", "Value", "data");
 end
